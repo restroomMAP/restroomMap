454b1a3c751aa44322d8a8459830259b326055d9
senzoo-oh <osj3418@gmail.com>
2023-07-19 11:02:31 +0900

    스프링부트 초기설정

---
+++ src/main/java/com/zalteam/restroomMap/RestroomMapApplication.java
@@ -0,0 +1,13 @@
+package com.zalteam.restroomMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestroomMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestroomMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zalteam/restroomMap/RestroomMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zalteam.restroomMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestroomMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

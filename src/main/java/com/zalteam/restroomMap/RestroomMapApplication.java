bf38f592f67382814c1a622b47ee77614d7c3003
senzoo-oh <osj3418@gmail.com>
2023-08-12 17:08:57 +0900

    feat: ✨ 리뷰 조회 기능

--- src/main/java/com/zalteam/restroomMap/RestroomMapApplication.java
+++ src/main/java/com/zalteam/restroomMap/RestroomMapApplication.java
@@ -1,12 +1,19 @@
 package com.zalteam.restroomMap;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class RestroomMapApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestroomMapApplication.class, args);
